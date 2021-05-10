ebe5bdd330d3c1529ddfc77c6744048b5f77073d
Michal S <michalsienczuk@gmail.com>
2021-05-10 21:40:34 +0200

    change layout

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.set('view engine', 'hbs');
 app.use(express.static(path.join(__dirname, '/public')));
 
 app.get('/', (req, res) => {
-    res.render('index', { layout: false });
+    res.render('index', );
 });
 
 app.get('/hello/:name', (req, res) => {
@@ -17,19 +17,19 @@ app.get('/hello/:name', (req, res) => {
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', { layout: false });
+    res.render('about', );
 });
 
 app.get('/contact', (req, res) => {
-    res.render('contact', { layout: false });
+    res.render('contact', );
 });
 
 app.get('/info', (req, res) => {
-    res.render('info', { layout: false });
+    res.render('info', );
 });
 
 app.get('/history', (req, res) => {
-    res.render('history', { layout: false });
+    res.render('history', );
 });
 
 app.use((req, res) => {
