b88c2de784754bddc94ca6a5de04425f3c334ca9
Michal S <michalsienczuk@gmail.com>
2021-05-11 10:51:16 +0200

    add dark layout

--- server.js
+++ server.js
@@ -2,9 +2,10 @@ const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
 
+
 const app = express();
-app.engine('hbs', hbs());
-app.set('view engine', 'hbs');
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 app.use(express.static(path.join(__dirname, '/public')));
 
@@ -13,12 +14,13 @@ app.get('/', (req, res) => {
 });
 
 app.get('/hello/:name', (req, res) => {
-    res.render('hello', { layout: false, name: req.params.name });
+    res.render('hello', { name: req.params.name });
 });
 
 app.get('/about', (req, res) => {
-    res.render('about', );
+    res.render('about', {layout: 'dark'});
 });
+
 
 app.get('/contact', (req, res) => {
     res.render('contact', );
